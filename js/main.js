c6c733347ceff404f03e4613edce18bd9931fd26
deeb <deeb.andrawis@gmail.com>
2015-08-26 01:26:10 +0300

    Quickactions + Loading data from json file

--- js/main.js
+++ js/main.js
@@ -1,40 +1,96 @@
-var app = angular.module('WebApp', []);
-app.controller('MainCtrl', ["$rootScope", "$scope", "$http", "$timeout", "UTILS", 
-	function($rootScope, $scope, $http, $timeout, UTILS){
+(function(angular) {
+  	'use strict';
+  	angular.module('WebApp', ['ngSanitize']);
+	angular.module('WebApp').controller('MainCtrl', ["$rootScope", "$scope", "$timeout", "UTILS", 
+		function($rootScope, $scope, $timeout, UTILS){
 
-	$scope.selectedTab;   			// Default selected tab when page finish laod
-	$scope.notification;  			// Holding notification data
-	$scope.topButtons;	  			// Holding buttons description and links
-	$scope.tabs;	 	  			// Holding tabs data
-	$scope.appInitialized = false;	// Hide binding data before initlized
+		$scope.selectedTab;   			// Holding selected tab
+		$scope.notification;  			// Holding notification data
+		$scope.topButtons;	  			// Holding buttons description and links
+		$scope.tabs;	 	  			// Holding tabs data
+		$scope.appInitialized = false;	// Hide binding data before initlized
 
-	/**
-	 * Initialize application parameters and loading data from ajax
-	 */
-	$scope.initializeApp = function(){
-		$scope.selectedTab = 0;
-		$scope.notification = {message:"The data of UTF BI would be updated at 16:00 pm."};
-		$scope.topButtons = [];
-		$scope.tabs = [];
+		/**
+		 * Initialize application parameters and loading data from ajax
+		 */
+		$scope.initializeApp = function(){
+			// Start animating loader
+			UTILS.animatePageLoad(true);
 
 
+			$scope.selectedTab = 0;  // Default selected tab when page finish laod
+			$scope.notification = {};
+			$scope.topButtons = [];
+			$scope.tabs = [];
 
-		UTILS.animatePageLoad(function(){
-			$scope.appInitialized = true;
-		});
-	}
-	$scope.initializeApp();
 
-	/**
-	 * Change selected tab and initialize it's content
-	 * @param  {Number} tabNumber Tab number between 1-4
-	 */
-	$scope.selectTab = function(tabNumber){
-		if(UTILS.isNumber(tabNumber)){
-			$scope.selectTab = tabNumber;
-		}else{
-			// Select default tab
-			$scope.selectTab = 0;
+			// Load 
+			UTILS.loadJsonData(function(data){
+				if(data && data != null){
+					// Fill controller parameters from data
+					
+
+					// Check if there is notification to display
+					if(data.notification && data.notification.length > 0){
+						$scope.notification={
+							message: data.notification,
+							style:{}
+						}
+					}
+
+					// Fill quick actions (top buttons) and modify the icons and actions label
+					if(data.quickActions && data.quickActions.length > 0){
+						$scope.topButtons = data.quickActions;
+						for(var i = 0; i < $scope.topButtons.length; i++){
+							$scope.topButtons[i].icon = {
+								"background-image":"url(img/"+$scope.topButtons[i].icon+".png)"
+							}
+							$scope.topButtons[i].actionsLabel += '<div class="drop-arrow"><img src="img/arrow.png" /></div>';
+						}	
+					}
+
+					// Fill tabs 
+					// Get related tabs links
+					// Check if there is old stored content in local storage
+
+
+
+				}else{
+					// Ajax error
+					$scope.notification = {
+						message:"ERROR: Cannot reteive data from server!",
+						style:{
+							"color":"rgb(234, 47, 64)"
+						}
+					};
+				}
+
+				// Init focus tabs
+				$('.top-button > .drop-down-options > li:last-child a').live('blur', function(e){
+					$(this).parents(".top-button").removeClass('hovered');
+				});
+
+				// Animate page load
+				UTILS.animatePageLoad(false,function(){
+					$scope.appInitialized = true;
+				});
+			});
 		}
-	}
-}]);
+		$scope.initializeApp();
+
+		/**
+		 * Change selected tab and initialize it's content
+		 * @param  {Number} tabNumber Tab number between 1-4
+		 */
+		$scope.selectTab = function(tabNumber){
+			if(UTILS.isNumber(tabNumber)){
+				$scope.selectTab = tabNumber;
+			}else{
+				// Select default tab
+				$scope.selectTab = 0;
+			}
+		}
+	}]);
+})(window.angular);
+
+
--- js/plugins.js
+++ js/plugins.js
@@ -1,36 +1,122 @@
-app.factory('UTILS', ["$timeout", function($timeout){
+(function(angular) {
+  'use strict';
+	angular.module('WebApp').factory('UTILS', ["$timeout", "$http", function($timeout, $http){
 
-	return {
-		/**
-		 * Check if a given value is a plain Object
-		 *
-		 * @param  {*}       o Any value to be checked
-		 * @return {Boolean}   true if it's an Object
-		 */
-		isObject: function (o) {
-			var toString = Object.prototype.toString;
-			return (toString.call(o) === toString.call({}));
-		},
+		return {
+			/**
+			 * Check if a given value is a plain Object
+			 *
+			 * @param  {*}       o Any value to be checked
+			 * @return {Boolean}   true if it's an Object
+			 */
+			isObject: function (o) {
+				var toString = Object.prototype.toString;
+				return (toString.call(o) === toString.call({}));
+			},
 
-		/**
-		 * Check if a given value is a Number
-		 *
-		 * @param  {*}       o Any value to be checked
-		 * @return {Boolean}   true if it's a Number
-		 */
-		isNumber: function (o) {
-			if(o)
-				return (typeof o == 'number');
-			else
-				return false;
-		},
+			/**
+			 * Check if a given value is a Number
+			 *
+			 * @param  {*}       o Any value to be checked
+			 * @return {Boolean}   true if it's a Number
+			 */
+			isNumber: function (o) {
+				if(o)
+					return (typeof o == 'number');
+				else
+					return false;
+			},
 
-		animatePageLoad: function(callback){
-			$timeout(function(){$('#page-loader').css('width',"25%");$timeout(function(){$('#page-loader').css('width',"90%");$timeout(function(){$('#page-loader').css('width',"100%");$timeout(function(){
-				$('#page-loader').hide();
-				$('#main-container').removeClass('transparet');
-				callback();
-			},400);},200);},200);},200);
+			/**
+			 * Animate top line loader and then display page content
+			 * @param  {Number}   percentage Load percentage
+			 * @param  {Function} callback   Function to call after Done
+			 */
+			animatePageLoad: function(percentage, callback){
+				if(percentage){
+					$('#page-loader').css('width',percentage+"%");
+				}else{
+					$timeout(function(){$('#page-loader').css('width',"90%");$timeout(function(){$('#page-loader').css('width',"100%");$timeout(function(){
+						$('#page-loader').hide();
+						$('#main-container').removeClass('transparet');
+						callback();
+					},400);},200);},200);
+				}
+			},
+
+			/**
+			 * Load data from json file with angularjs ajax request
+			 * @param  {Function} callback Function to call after receiving data
+			 */
+			loadJsonData: function(callback){
+				$http.get('http://ghyonimor.github.io/webappjquery/data/config.json').then(
+					function(response) {
+						if(response.status == 200 && response.data)
+							callback(response.data);
+						else
+							callback(null);
+					}, function(response) {
+						callback(null);
+					}
+				);
+			}
 		}
-	}
-}]);
+	}])
+
+	/**
+	 * Handle Enter keydown when focusing quickaction button
+	 */
+	.directive('ngEnterHover', function() {
+		return {
+			restrict: 'A',
+			link: function($scope,elem,attrs) {
+				elem.bind('keydown', function(e) {
+					var code = e.keyCode || e.which;
+					if (code === 13) {
+						$('.top-button').removeClass("hovered");
+						$(elem).addClass('hovered');
+						e.preventDefault();
+					}
+				});
+			}
+		}
+	})
+	/**
+	 * Handle mouse over on other quickaction button while exploring there actions
+	 */
+	.directive('ngHover', function() {
+		return {
+			restrict: 'A',
+			link: function($scope,elem,attrs) {
+				$(elem).on('mouseover', function(e){
+					$('.top-button').removeClass("hovered");
+					$('.top-button').blur();
+				});
+			}
+		}
+	})
+	/**
+	 * Handle Enter keydown when focusing quickaction menu action to trigger click
+	 */
+	.directive('ngEnterLink', function() {
+		return {
+			restrict: 'A',
+			link: function($scope,elem,attrs) {
+				elem.bind('keydown', function(e) {
+					var code = e.keyCode || e.which;
+					if (code === 13) {
+						$(elem)[0].click();
+						e.preventDefault();
+					}
+				});
+			}
+		}
+	});
+})(window.angular);
+
+
+
+
+
+
+
